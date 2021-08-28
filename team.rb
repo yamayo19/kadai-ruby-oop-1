d721a916ef664bc548b296120ff3464809399847
yosuke.yamamoto <ymmtysk19@gmail.com>
2021-08-28 20:13:43 +0000

    team.rb submit

---
+++ team.rb
@@ -0,0 +1,39 @@
+# クラス定義
+class Team
+
+  # インスタンスが持つ変数（値）
+  attr_accessor :name, :win, :lose, :draw
+
+  # インスタンスを初期化するための、特別なメソッド
+  def initialize(team,w,l,d)
+    
+    self.name = team
+    self.win = w
+    self.lose = l
+    self.draw = d
+  end
+
+  # インスタンスが持つメソッド（処理）
+  def calc_win_rate(win,lose)
+    calc_win_rate = win.to_f/(win.to_f + lose.to_f)
+  end
+  def show_team_result
+    # self.type ではなく、 self.name を呼び出す
+    puts "#{self.name}の2020年の成績は#{self.win}勝 #{self.lose}敗 #{self.draw}分、 勝率は#{calc_win_rate(self.win,self.lose)}です。"
+  end
+end
+# インスタンスの生成と、変数への代入
+giants = Team.new("Giants",67,45,8)
+tigers = Team.new("Tigers",60,53,7)
+dragons = Team.new("Dragons",60,55,5)
+baystars = Team.new("Baystars",56,58,6)
+carp = Team.new("Carp",52,56,12)
+swarrows = Team.new("Swarrows",41,69,10)
+
+# インスタンスの使用
+giants.show_team_result
+tigers.show_team_result
+dragons.show_team_result
+baystars.show_team_result
+carp.show_team_result
+swarrows.show_team_result
